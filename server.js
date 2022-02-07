1746042b76c2e828cd262813b5c70ed334f8a5f5
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-02-07 13:29:26 -0600

    connected the table, created a query method to display data

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username,
+        user: 'root',
+        //your mysql passwird
+        password: 'H0m3{wADT};',
+        database: 'election'
+    }, 
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 //default response for any other request (Not Found)
 app.use((req, res) => {
